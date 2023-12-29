65bf3a0e2a49a544348b903f3933c33c9c8fbe94
GeorgiyIakunenko <Goshan3097@gmail.com>
2023-12-29 02:36:30 +0100

    feat: prettier with config added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import {Welcome} from "@/pages/Welcome.tsx";
+import { Welcome } from '@/pages/Welcome.tsx';
 
-import './index.css'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Welcome/>
+    <Welcome />
   </React.StrictMode>,
-)
+);
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,3 @@
 export function Welcome() {
-  return (
-    <>Welcome</>
-  );
+  return <>Welcome</>;
 }
