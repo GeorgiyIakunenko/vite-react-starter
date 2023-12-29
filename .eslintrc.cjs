6ba4bada8e7e60363ff1251031fc80584515bd34
GeorgiyIakunenko <Goshan3097@gmail.com>
2023-12-29 02:06:35 +0100

    feat(eslint-imports): eslint import plugin and rules added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,12 +12,42 @@ module.exports = {
     ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh','@typescript-eslint', 'react'],
+    plugins: ['react-refresh','@typescript-eslint', 'simple-import-sort', 'react'],
     rules: {
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true },
         ],
         'react/react-in-jsx-scope': 'off', // React 17 no longer requires importing React
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
     },
+    "overrides": [
+        {
+            "files": ["**/*.js", "**/*.ts", "**/*.tsx"],
+            "rules": {
+                "simple-import-sort/imports": [
+                    "error",
+                    {
+                        "groups": [
+                            // `react` first, `next` second, then packages starting with a character
+                            ["^react$", "^next", "^[a-z]"],
+                            // Packages starting with `@`
+                            ["^@"],
+                            // Packages starting with `~`
+                            ["^~"],
+                            // Imports starting with `../`
+                            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+                            // Imports starting with `./`
+                            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+                            // Style imports
+                            ["^.+\\.s?css$"],
+                            // Side effect imports
+                            ["^\\u0000"]
+                        ]
+                    }
+                ]
+            }
+        }
+    ],
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+
 import {Welcome} from "@/pages/Welcome.tsx";
+
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
